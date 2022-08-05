1667019e47476da62d608027b56aa3e282934ea9
Hiren Odedara <hireno@zeta.tech>
2022-08-05 14:22:54 +0530

    Adding Spring initializr UI and Zeta Dependency

---
+++ aether-spring-app/src/main/java/in/zeta/aether/spring/initializr/rest/controller/HomeController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package in.zeta.aether.spring.initializr.rest.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Main Controller.
+ *
+ * @author Brian Clozel
+ */
+@Controller
+public class HomeController {
+
+	@GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+	public String home() {
+		System.out.println("Wow Hiren");
+		return "forward:index.html";
+	}
+
+}
