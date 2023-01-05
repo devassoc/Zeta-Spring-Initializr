0f3d3134f5b7b327d5ce3c4101f402e67f1cc1ce
devassoc <preetish@zeta.tech>
2023-01-05 14:49:32 +0530

    Logging and monitoring added

--- zeta-spring-initializr-app/src/main/java/in/zeta/spring/initializr/app/ZetaSpringInitializrApp.java
+++ zeta-spring-initializr-app/src/main/java/in/zeta/spring/initializr/app/ZetaSpringInitializrApp.java
@@ -1,5 +1,6 @@
 package in.zeta.spring.initializr.app;
 
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
